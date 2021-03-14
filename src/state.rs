56fd082db1d747553213e5ac2dbba64ab31431c0
Cosmic Punk <cosmospunk1@gmail.com>
2021-03-13 23:41:40 -0500

    remove unused types

--- src/state.rs
+++ src/state.rs
@@ -8,12 +8,6 @@ use cw_storage_plus::{Item, Map};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
-}
-
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 pub struct Offering {
     pub token_id: String,
@@ -34,12 +28,4 @@ pub fn increment_offerings(storage: &mut Storage) -> StdResult<u64> {
     let val = num_offerings(storage)? + 1;
     OFFERINGS_COUNT.save(storage, &val)?;
     Ok(val)
-}
-
-pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
-    singleton(storage, CONFIG_KEY)
-}
-
-pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
-    singleton_read(storage, CONFIG_KEY)
 }
